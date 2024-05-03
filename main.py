f5cc8148c6f46293e6fc71efe633980f8399c67b
Mkl-Manowar <151755301+Mkl-Manowar@users.noreply.github.com>
2024-05-03 17:09:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,13 @@
+class usuario:
+  def __init__(self, username, password):
+    self.username = username
+    self.password = password
+
+get_user = usuario("Mikel", "1234")
+
+print(get_user)
+print(get_user.username)
+print(get_user.password)
+
+
+    
